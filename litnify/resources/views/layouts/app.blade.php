778cd5aed27dc0172cbac06c2b6f387c5c388df8
svnwa <62885174+svnwa@users.noreply.github.com>
2020-11-08 13:05:14 +0100

    Nachricht <div> für User => Über session oder View-Paramenter übergeben

--- litnify/resources/views/layouts/app.blade.php
+++ litnify/resources/views/layouts/app.blade.php
@@ -9,6 +9,19 @@
 <div id="app">
 
     @include('layouts.nav')
+    @if (isset($alert))
+        <div class="container">
+            <div class="alert alert-{{$alert[0]}}">
+                {{ $alert[1] }}
+            </div>
+        </div>
+    @elseif(session('alert'))
+        <div class="container">
+            <div class="alert alert-{{session('alert')[0]}}">
+                {{ session('alert')[1] }}
+            </div>
+        </div>
+    @endif
 
     <main class="" style="margin-top:66px; background-color: #ffffff; min-height: 80vh;" >
         @yield('content')
