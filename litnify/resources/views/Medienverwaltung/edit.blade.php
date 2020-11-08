6c7ae6d04efcff36e9f551a47f5e1931a04e1e83
svnwa <62885174+svnwa@users.noreply.github.com>
2020-11-08 13:06:24 +0100

    Medium Bearbeitung: Unterscheidung der Inputs nach Literaturart

--- litnify/resources/views/Medienverwaltung/edit.blade.php
+++ litnify/resources/views/Medienverwaltung/edit.blade.php
@@ -36,15 +36,17 @@
                     @enderror
                 </div>
 
-                <div class="form-group">
-                    <label for="signatur">Signatur</label>
-                    <input type="text"
-                           class="form-control @error('signatur') border-danger @enderror" name="signatur" id="signatur"
-                           value="{{$medium->signatur}}">
-                    @error('signatur')
-                        <div class="invalid-feedback d-block">{{$message}}</div>
-                    @enderror
-                </div>
+                @if($literaturart=='Buch'xor'Graue Literatur'xor'Unselbstständiges Werk')
+                    <div class="form-group">
+                        <label for="signatur">Signatur</label>
+                        <input type="text"
+                               class="form-control @error('signatur') border-danger @enderror" name="signatur" id="signatur"
+                               value="{{$medium->signatur}}">
+                        @error('signatur')
+                            <div class="invalid-feedback d-block">{{$message}}</div>
+                        @enderror
+                    </div>
+                @endif
 
                 <livewire:autoren-component :medium="$medium" />
 
@@ -68,25 +70,29 @@
                     @enderror
                 </div>
 
-                <div class="form-group">
-                    <label for="enthalten_in">Enthalten in</label>
-                    <input type="text"
-                           class="form-control @error('enthalten_in') border-danger @enderror" name="enthalten_in" id="enthalten_in" aria-describedby="helpId"
-                           value="{{$medium->enthalten_in}}">
-                    @error('enthalten_in')
-                        <div class="invalid-feedback d-block">{{$message}}</div>
-                    @enderror
-                </div>
+                @if($literaturart=='Unselbstständiges Werk')
+                    <div class="form-group">
+                        <label for="enthalten_in">Enthalten in</label>
+                        <input type="text"
+                               class="form-control @error('enthalten_in') border-danger @enderror" name="enthalten_in" id="enthalten_in" aria-describedby="helpId"
+                               value="{{$medium->enthalten_in}}">
+                        @error('enthalten_in')
+                            <div class="invalid-feedback d-block">{{$message}}</div>
+                        @enderror
+                    </div>
+                @endif
 
-                <div class="form-group">
-                    <label for="erscheinungsort">Erscheinungsort</label>
-                    <input type="text"
-                           class="form-control @error('erscheinungsort') border-danger @enderror" name="erscheinungsort" id="erscheinungsort"
-                           value="{{$medium->erscheinungsort}}">
-                    @error('erscheinungsort')
-                        <div class="invalid-feedback d-block">{{$message}}</div>
-                    @enderror
-                </div>
+                @if($literaturart=='Buch'xor'Graue Literatur'xor'Unselbstständiges Werk'xor'Daten')
+                    <div class="form-group">
+                        <label for="erscheinungsort">Erscheinungsort</label>
+                        <input type="text"
+                               class="form-control @error('erscheinungsort') border-danger @enderror" name="erscheinungsort" id="erscheinungsort"
+                               value="{{$medium->erscheinungsort}}">
+                        @error('erscheinungsort')
+                            <div class="invalid-feedback d-block">{{$message}}</div>
+                        @enderror
+                    </div>
+                @endif
 
                 <div class="form-group">
                     <label for="jahr">Jahr</label>
@@ -98,25 +104,29 @@
                     @enderror
                 </div>
 
-                <div class="form-group">
-                    <label for="verlag">Verlag</label>
-                    <input type="text"
-                           class="form-control @error('verlag') border-danger @enderror" name="verlag" id="verlag"
-                           value="{{$medium->verlag}}">
-                    @error('verlag')
-                        <div class="invalid-feedback d-block">{{$message}}</div>
-                    @enderror
-                </div>
+                @if($literaturart=='Buch'xor'Graue Literatur'xor'Unselbstständiges Werk'xor'Daten')
+                    <div class="form-group">
+                        <label for="verlag">Verlag</label>
+                        <input type="text"
+                               class="form-control @error('verlag') border-danger @enderror" name="verlag" id="verlag"
+                               value="{{$medium->verlag}}">
+                        @error('verlag')
+                            <div class="invalid-feedback d-block">{{$message}}</div>
+                        @enderror
+                    </div>
+                @endif
 
-                <div class="form-group">
-                    <label for="isbn">ISBN</label>
-                    <input type="text"
-                           class="form-control @error('isbn') border-danger @enderror" name="isbn" id="isbn"
-                           value="{{$medium->isbn}}">
-                    @error('isbn')
-                        <div class="invalid-feedback d-block">{{$message}}</div>
-                    @enderror
-                </div>
+                @if($literaturart=='Buch'xor'Graue Literatur'xor'Unselbstständiges Werk')
+                    <div class="form-group">
+                        <label for="isbn">ISBN</label>
+                        <input type="text"
+                               class="form-control @error('isbn') border-danger @enderror" name="isbn" id="isbn"
+                               value="{{$medium->isbn}}">
+                        @error('isbn')
+                            <div class="invalid-feedback d-block">{{$message}}</div>
+                        @enderror
+                    </div>
+                @endif
 
                 <div class="form-group">
                     <label for="issn">ISSN</label>
@@ -128,32 +138,49 @@
                     @enderror
                 </div>
 
-                <label for="inventarnummern ">Inventarnummern</label>
-                <button id="inventarnummern" type="button" class="btn btn-outline-secondary btn-block mb-3" data-toggle="modal" data-target="#modelId">Inventarnummern</button>
+                @if($literaturart=='Artikel')
+                    <div class="form-group">
+                        <label for="doi">DOI</label>
+                        <input type="text"
+                               class="form-control @error('doi') border-danger @enderror" name="doi" id="doi"
+                               placeholder=""
+                               value="{{$medium->issn}}"
+                        >
+                        @error('doi')
+                            <div class="invalid-feedback d-block">{{$message}}</div>
+                        @enderror
+                    </div>
+                @endif
 
-                <!-- Modal -->
+                @if($literaturart=='Buch'xor'Graue Literatur'xor'Unselbstständiges Werk')
 
-                <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
-                     aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title">Inventarnummern</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="container-fluid">
-                                    <livewire:inventarnummern-component :medium="$medium" />
+                    <label for="inventarnummern ">Inventarnummern</label>
+                    <button id="inventarnummern" type="button" class="btn btn-outline-secondary btn-block mb-3" data-toggle="modal" data-target="#modelId">Inventarnummern</button>
+
+                    <!-- Modal -->
+
+                    <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
+                         aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Inventarnummern</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
                                 </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fenster schließen</button>
+                                <div class="modal-body">
+                                    <div class="container-fluid">
+                                        <livewire:inventarnummern-component :medium="$medium" />
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fenster schließen</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                @endif
 
                 {{--<div class="form-group">
                     <label for="inventarnummer">Inventarnummer</label>
@@ -165,55 +192,63 @@
                     @enderror
                 </div>--}}
 
-                <div class="form-group">
-                    <label for="auflage">Auflage</label>
-                    <input type="text"
-                           class="form-control @error('auflage') border-danger @enderror" name="auflage" id="auflage"
-                           value="{{$medium->auflage}}">
-                    @error('auflage')
-                        <div class="invalid-feedback d-block">{{$message}}</div>
-                    @enderror
-                </div>
+                @if($literaturart=='Buch'xor'Graue Literatur'xor'Unselbstständiges Werk')
+                    <div class="form-group">
+                        <label for="auflage">Auflage</label>
+                        <input type="text"
+                               class="form-control @error('auflage') border-danger @enderror" name="auflage" id="auflage"
+                               value="{{$medium->auflage}}">
+                        @error('auflage')
+                            <div class="invalid-feedback d-block">{{$message}}</div>
+                        @enderror
+                    </div>
+                @endif
 
-                <div class="form-group">
-                    <label for="herausgeber">Herausgeber</label>
-                    <input type="text"
-                           class="form-control @error('herausgeber') border-danger @enderror" name="herausgeber" id="herausgeber"
-                           value="{{$medium->herausgeber}}">
-                    @error('herausgeber')
-                        <div class="invalid-feedback d-block">{{$message}}</div>
-                    @enderror
-                </div>
+                @if($literaturart=='Buch'xor'Graue Literatur'xor'Unselbstständiges Werk'xor'Daten')
+                    <div class="form-group">
+                        <label for="herausgeber">Herausgeber</label>
+                        <input type="text"
+                               class="form-control @error('herausgeber') border-danger @enderror" name="herausgeber" id="herausgeber"
+                               value="{{$medium->herausgeber}}">
+                        @error('herausgeber')
+                            <div class="invalid-feedback d-block">{{$message}}</div>
+                        @enderror
+                    </div>
+                @endif
 
-                <div class="form-group">
-                    <label for="zeitschrift_id">Zeitschrift</label>
-                    {{--TODO mit Datalist austauschen--}}
-                    <select class="form-control" name="zeitschrift_id" id="zeitschrift_id">
-                        @foreach(App\Models\Zeitschrift::all() as $zeitschrift)
-                            @if($medium->zeitschrift_id==$zeitschrift->name)
-                                <option value="{{$zeitschrift->name}}" selected>{{$zeitschrift->name}} [{{$zeitschrift->shortcut}}]</option>
-                            @else
-                                <option value="{{$zeitschrift->name}}">{{$zeitschrift->name}} [{{$zeitschrift->shortcut}}]</option>
-                            @endif
-                        @endforeach
-                    </select>
-{{--                    <input type="text"--}}
-{{--                           class="form-control @error('zeitschrift_id') border-danger @enderror" name="zeitschrift_id" id="zeitschrift_id" aria-describedby="helpId"--}}
-{{--                           value="{{$medium->zeitschrift_id}}">--}}
-                    @error('zeitschrift_id')
-                        <div class="invalid-feedback d-block">{{$message}}</div>
-                    @enderror
-                </div>
+                @if($literaturart=='Artikel')
+                    <div class="form-group">
+                        <label for="zeitschrift_id">Zeitschrift</label>
+                        {{--TODO mit Datalist austauschen ??--}}
+                        <select class="form-control" name="zeitschrift_id" id="zeitschrift_id">
+                            @foreach(App\Models\Zeitschrift::all() as $zeitschrift)
+                                @if($medium->zeitschrift_id==$zeitschrift->name)
+                                    <option value="{{$zeitschrift->name}}" selected>{{$zeitschrift->name}} [{{$zeitschrift->shortcut}}]</option>
+                                @else
+                                    <option value="{{$zeitschrift->name}}">{{$zeitschrift->name}} [{{$zeitschrift->shortcut}}]</option>
+                                @endif
+                            @endforeach
+                        </select>
+    {{--                    <input type="text"--}}
+    {{--                           class="form-control @error('zeitschrift_id') border-danger @enderror" name="zeitschrift_id" id="zeitschrift_id" aria-describedby="helpId"--}}
+    {{--                           value="{{$medium->zeitschrift_id}}">--}}
+                        @error('zeitschrift_id')
+                            <div class="invalid-feedback d-block">{{$message}}</div>
+                        @enderror
+                    </div>
+                @endif
 
-                <div class="form-group">
-                    <label for="schriftenreihe">Schriftenreihe</label>
-                    <input type="text"
-                           class="form-control @error('schriftenreihe') border-danger @enderror" name="schriftenreihe" id="schriftenreihe"
-                           value="{{$medium->schriftenreihe}}">
-                    @error('schriftenreihe')
-                        <div class="invalid-feedback d-block">{{$message}}</div>
-                    @enderror
-                </div>
+                @if($literaturart=='Buch'xor'Graue Literatur'xor'Unselbstständiges Werk'xor'Daten')
+                    <div class="form-group">
+                        <label for="schriftenreihe">Schriftenreihe</label>
+                        <input type="text"
+                               class="form-control @error('schriftenreihe') border-danger @enderror" name="schriftenreihe" id="schriftenreihe"
+                               value="{{$medium->schriftenreihe}}">
+                        @error('schriftenreihe')
+                            <div class="invalid-feedback d-block">{{$message}}</div>
+                        @enderror
+                    </div>
+                @endif
 
                 <div class="form-group">
                     <label for="band">Band</label>
@@ -234,6 +269,20 @@
                         <div class="invalid-feedback d-block">{{$message}}</div>
                     @enderror
                 </div>
+
+                @if($literaturart=='Graue Literatur'xor'Unselbstständiges Werk'xor'Daten')
+                    <div class="form-group">
+                        <label for="institut">Institut</label>
+                        <input type="text"
+                               class="form-control @error('institut') border-danger @enderror" name="institut" id="institut"
+                               placeholder=""
+                               value="{{$medium->institut}}"
+                        >
+                        @error('institut')
+                        <div class="invalid-feedback d-block">{{$message}}</div>
+                        @enderror
+                    </div>
+                @endif
 
                 <div class="form-group">
                     <label for="raum_id">Standort</label>
