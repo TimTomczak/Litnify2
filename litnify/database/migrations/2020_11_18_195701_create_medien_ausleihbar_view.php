909e19e51f2fb687b4d9da94eef9f783efee91c8
svnwa <62885174+svnwa@users.noreply.github.com>
2020-11-30 19:50:49 +0100

    Migration für View von ausleihbaren Medien angepasst

--- litnify/database/migrations/2020_11_18_195701_create_medien_ausleihbar_view.php
+++ litnify/database/migrations/2020_11_18_195701_create_medien_ausleihbar_view.php
@@ -14,10 +14,11 @@ class CreateMedienAusleihbarView extends Migration
     public function up()
     {
         DB::statement("CREATE VIEW ".env('DB_TABLE_PREFIX','laravel')."medien_ausleihbar AS
-                                SELECT inv.*
+                                SELECT inv.*,med.literaturart_id,med.signatur,med.autoren,med.hauptsachtitel,med.jahr,med.zeitschrift_id,med.raum_id
                                 FROM ".env('DB_TABLE_PREFIX','laravel')."ausleihen aus
                                 RIGHT JOIN ".env('DB_TABLE_PREFIX','laravel')."inventarliste inv
                                 ON aus.medium_id=inv.medium_id AND aus.inventarnummer=inv.inventarnummer
+                                LEFT JOIN ".env('DB_TABLE_PREFIX','laravel')."medien med ON med.id=inv.medium_id
                                 WHERE inv.ausleihbar=1
                                 AND aus.id is null
                                 OR aus.RueckgabeIst IS NOT null
