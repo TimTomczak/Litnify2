8f966c93f886908bd191d355a60a33a9a32e618f
svnwa <62885174+svnwa@users.noreply.github.com>
2020-11-30 20:01:39 +0100

    Routen angepasst: Keine Resource-Routen mehr (wegen auth-middleware), 'admin'-Präfix für Administrationsrouten

--- litnify/routes/web.php
+++ litnify/routes/web.php
@@ -40,53 +40,53 @@ Route::get('/user/merkliste', [App\Http\Controllers\UserController::class, 'show
 /***********************************/
 /*        Medienverwaltung         */
 /***********************************/
-Route::get('medienverwaltung', [App\Http\Controllers\MediumController::class, 'index'])->name('medienverwaltung.index');
-Route::get('medium/{medium}', [App\Http\Controllers\MediumController::class, 'show'])->name('medium.show')->where(array('medium' => '[0-9]+'));;
-Route::resource('medienverwaltung/medium', MediumController::class)->only(['edit', 'store', 'update', 'destroy'])->where(array('medium' => '[0-9]+'));
-Route::resource('medienverwaltung/medium',  App\Http\Controllers\MediumController::class)->only([
-    'edit',  'store', 'update', 'destroy'
-])->where(array('medium' => '[0-9]+'));
-Route::get('medienverwaltung/medium/create', [App\Http\Controllers\MediumController::class, 'create'])->name('medium.createEmpty');
-Route::get('medienverwaltung/medium/create/{literaturart}', [App\Http\Controllers\MediumController::class, 'create'])->name('medium.create');
+Route::get('medium/{medium}', [App\Http\Controllers\MediumController::class, 'show'])->name('medium.show')->where(array('medium' => '[0-9]+'))->middleware('auth', 'role:2');
 Route::get('autor/{autor}', [App\Http\Controllers\MediumController::class, 'showAutor'])->name('autor.show');
 
-/*  Freigabe    */
-Route::get('medienverwaltung/freigabe', [App\Http\Controllers\FreigabeController::class, 'index'])->name('freigabe.index');
-Route::put('medienverwaltung/{medium}/freigabe', [App\Http\Controllers\FreigabeController::class, 'update'])->name('freigabe.update')->where(array('medium' => '[0-9]+'));
+Route::prefix('admin')->group(function() {
+    Route::get('medienverwaltung', [App\Http\Controllers\MediumController::class, 'index'])->name('medienverwaltung.index');
+    Route::get('medienverwaltung/medium/create', [App\Http\Controllers\MediumController::class, 'create'])->name('medium.createEmpty')->middleware('auth', 'role:2');
+    Route::get('medienverwaltung/medium/create/{literaturart}', [App\Http\Controllers\MediumController::class, 'create'])->name('medium.create')->middleware('auth', 'role:2');
+    Route::post('medienverwaltung/medium', [App\Http\Controllers\MediumController::class, 'store'])->name('medium.store')->where(array('medium' => '[0-9]+'))->middleware('auth', 'role:2');
+    Route::get('medienverwaltung/medium/{medium}/edit', [App\Http\Controllers\MediumController::class, 'edit'])->name('medium.edit')->where(array('medium' => '[0-9]+'))->middleware('auth', 'role:2');
+    Route::put('medienverwaltung/medium/{medium}', [App\Http\Controllers\MediumController::class, 'update'])->name('medium.update')->where(array('medium' => '[0-9]+'))->middleware('auth', 'role:2');
+    Route::delete('medienverwaltung/medium/{medium}', [App\Http\Controllers\MediumController::class, 'destroy'])->name('medium.destroy')->where(array('medium' => '[0-9]+'))->middleware('auth', 'role:3');
+    /*  Freigabe    */
+    Route::get('medienverwaltung/freigabe', [App\Http\Controllers\FreigabeController::class, 'index'])->name('freigabe.index')->middleware('auth', 'role:2');
+    Route::put('medienverwaltung/{medium}/freigabe', [App\Http\Controllers\FreigabeController::class, 'update'])->name('freigabe.update')->where(array('medium' => '[0-9]+'))->middleware('auth', 'role:3');
+    /*  Zeitschriftenverwaltung */
+    Route::get('zeitschriftenverwaltung', [App\Http\Controllers\ZeitschriftController::class, 'index'])->name('zeitschriften.index')->middleware('auth', 'role:2');
+    Route::get('zeitschriftenverwaltung/zeitschrift/{zeitschrift}/edit', [App\Http\Controllers\ZeitschriftController::class, 'edit'])->name('zeitschrift.edit')->middleware('auth', 'role:2');
+    Route::get('zeitschriftenverwaltung/zeitschrift/create', [App\Http\Controllers\ZeitschriftController::class, 'create'])->name('zeitschrift.create')->middleware('auth', 'role:2');
+    Route::post('zeitschriftenverwaltung/zeitschrift', [App\Http\Controllers\ZeitschriftController::class, 'store'])->name('zeitschrift.store')->middleware('auth', 'role:2');
+    Route::put('zeitschriftenverwaltung/zeitschrift/{zeitschrift}', [App\Http\Controllers\ZeitschriftController::class, 'update'])->name('zeitschrift.update')->middleware('auth', 'role:2');
+    Route::delete('zeitschriftenverwaltung/zeitschrift/{zeitschrift}', [App\Http\Controllers\ZeitschriftController::class, 'destroy'])->name('zeitschrift.destroy')->middleware('auth', 'role:2');
+    /*  Inventarliste   */
+    /*   Inventarliste wird über die Lifewire Component verwaltet. Siehe: \App\Http\Livewire\InventarnummernComponent::class    */
 
-/*  Zeitschriftenverwaltung */
-Route::get('zeitschriftenverwaltung', [App\Http\Controllers\ZeitschriftController::class, 'index'])->name('zeitschriften.index');
-Route::resource('medienverwaltung/zeitschrift', App\Http\Controllers\ZeitschriftController::class)->only([
-    'edit', 'create', 'store', 'update', 'destroy'
-])->where(array('zeitschrift' => '[0-9]+'));
-/*  Inventarliste   */
-/*   Inventarliste wird über die Lifewire Component verwaltet. Siehe: \App\Http\Livewire\InventarnummernComponent::class    */
-
-/***********************************/
-/*        Ausleihverwaltung        */
-/***********************************/
-Route::get('ausleihverwaltung', [App\Http\Controllers\AusleiheController::class, 'index'])->name('ausleihverwaltung.index');
-Route::post('ausleihverwaltung/ausleihe/{user}/{medium}', [App\Http\Controllers\AusleiheController::class, 'store'])->where(array('user' => '[0-9]+', 'medium' => '[0-9]+'))->name('ausleihe.store');
-Route::get('ausleihverwaltung/ausleihen/{ausleihe}', [App\Http\Controllers\AusleiheController::class, 'show'])->where(array('user' => '[0-9]+'))->name('ausleihe.show');
-Route::resource('ausleihverwaltung/ausleihe', App\Http\Controllers\AusleiheController::class)->only([
-    'edit', 'update', 'destroy'
-])->where(array('ausleihe' => '[0-9]+'));
-Route::put('ausleihverwaltung/ausleihe/{ausleihe}/verlaengern', [App\Http\Controllers\AusleiheController::class, 'updateVerlaegerungen'])->where(array('ausleihe' => '[0-9]+'))->name('ausleihe.extend');
-Route::put('ausleihverwaltung/ausleihe/{ausleihe}/rueckgabe', [App\Http\Controllers\AusleiheController::class, 'updateRueckgabe'])->where(array('ausleihe' => '[0-9]+'))->name('ausleihe.return');
-
-/*  Direktverleih   */
-Route::get('ausleihverwaltung/direktverleih', [App\Http\Controllers\DirektverleihController::class, 'index'])->name('direktverleih.index');
-Route::get('ausleihverwaltung/direktverleih/create/{user}', [App\Http\Controllers\DirektverleihController::class, 'create'])->name('direktverleih.create');
-
-/*  Merklistenverleih   */
-Route::get('ausleihverwaltung/merklistenverleih', [App\Http\Controllers\MerklistenverleihController::class, 'index'])->name('merklistenverleih.index');
-Route::get('ausleihverwaltung/merklistenverleih/{user}', [App\Http\Controllers\MerklistenverleihController::class, 'show'])->where(array('user' => '[0-9]+'))->name('merklistenverleih.show');
+    /***********************************/
+    /*        Ausleihverwaltung        */
+    /***********************************/
+    Route::get('ausleihverwaltung', [App\Http\Controllers\AusleiheController::class, 'index'])->name('ausleihverwaltung.index')->middleware('auth', 'role:2');
+    Route::post('ausleihverwaltung/ausleihe/{user}/{medium}', [App\Http\Controllers\AusleiheController::class, 'store'])->where(array('user' => '[0-9]+', 'medium' => '[0-9]+'))->name('ausleihe.store')->middleware('auth', 'role:3');
+    Route::get('ausleihverwaltung/ausleihen/{ausleihe}', [App\Http\Controllers\AusleiheController::class, 'show'])->where(array('user' => '[0-9]+'))->name('ausleihe.show')->middleware('auth', 'role:2');
+    Route::get('ausleihverwaltung/ausleihen/{ausleihe}/edit', [App\Http\Controllers\AusleiheController::class, 'edit'])->where(array('user' => '[0-9]+'))->name('ausleihe.edit')->middleware('auth', 'role:3');
+    Route::put('ausleihverwaltung/ausleihen/{ausleihe}', [App\Http\Controllers\AusleiheController::class, 'update'])->where(array('user' => '[0-9]+'))->name('ausleihe.update')->middleware('auth', 'role:3');
+    Route::delete('ausleihverwaltung/ausleihen/{ausleihe}', [App\Http\Controllers\AusleiheController::class, 'destroy'])->where(array('user' => '[0-9]+'))->name('ausleihe.destroy')->middleware('auth', 'role:3');
+    Route::put('ausleihverwaltung/ausleihe/{ausleihe}/verlaengern', [App\Http\Controllers\AusleiheController::class, 'updateVerlaegerungen'])->where(array('ausleihe' => '[0-9]+'))->name('ausleihe.extend')->middleware('auth', 'role:3');
+    Route::put('ausleihverwaltung/ausleihe/{ausleihe}/rueckgabe', [App\Http\Controllers\AusleiheController::class, 'updateRueckgabe'])->where(array('ausleihe' => '[0-9]+'))->name('ausleihe.return')->middleware('auth', 'role:3');
+    /*  Direktverleih   */
+    Route::get('ausleihverwaltung/direktverleih', [App\Http\Controllers\DirektverleihController::class, 'index'])->name('direktverleih.index')->middleware('auth', 'role:3');
+    Route::get('ausleihverwaltung/direktverleih/create/{user}', [App\Http\Controllers\DirektverleihController::class, 'create'])->name('direktverleih.create')->middleware('auth', 'role:3');
+    /*  Merklistenverleih   */
+    Route::get('ausleihverwaltung/merklistenverleih', [App\Http\Controllers\MerklistenverleihController::class, 'index'])->name('merklistenverleih.index')->middleware('auth', 'role:3');
+    Route::get('ausleihverwaltung/merklistenverleih/{user}', [App\Http\Controllers\MerklistenverleihController::class, 'show'])->where(array('user' => '[0-9]+'))->name('merklistenverleih.show')->middleware('auth', 'role:3');
+});
 
 // * A d m i n P a g e  s * //
 Route::group(['prefix' => 'admin',  'middleware' => 'auth'], function(){
 
     Route::get('nutzerverwaltung', [App\Http\Controllers\UserController::class, 'index'])->name('admin.nutzerverwaltung');
-    Route::get('ausleihverwaltung', [App\Http\Controllers\UserController::class, 'index'])->name('admin.ausleihverwaltung');
     Route::get('systemverwaltung', [App\Http\Controllers\Admin\SystemController::class, 'index'])->name('admin.systemverwaltung')->middleware('auth', 'role:4');
     Route::get('systemverwaltung/auswertungen', [App\Http\Controllers\Admin\SystemController::class, 'auswertungen'])->name('admin.systemverwaltung.auswertungen')->middleware('auth', 'role:4');
     Route::get('systemverwaltung/contenteditor', [App\Http\Controllers\Admin\SystemController::class, 'contentEditor'])->name('admin.systemverwaltung.contenteditor')->middleware('auth', 'role:4');
