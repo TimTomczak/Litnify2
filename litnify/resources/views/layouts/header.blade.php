2b5e8b782718e8a51e9ab15c3fce0a7fe3f509c5
svnwa <62885174+svnwa@users.noreply.github.com>
2020-12-07 17:05:28 +0100

    asset toast-position.css wieder entfernt

--- litnify/resources/views/layouts/header.blade.php
+++ litnify/resources/views/layouts/header.blade.php
@@ -26,7 +26,6 @@
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-<link href="{{ asset('css/toast-position.css') }}" rel="stylesheet">
 
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
