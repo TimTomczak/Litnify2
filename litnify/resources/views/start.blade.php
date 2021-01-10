e9f2fa76a13b931611ccca3e97609096a6ce6fd8
TimTomczak <41620700+TimTomczak@users.noreply.github.com>
2021-01-10 19:53:36 +0100

    #46 - Responsive Startseite Suchfeld

--- litnify/resources/views/start.blade.php
+++ litnify/resources/views/start.blade.php
@@ -1,19 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <style>
-        header {
-            height: 86vh;
-            min-height: 500px;
-            background-image: url('{{asset('storage/images/background.png')}}');
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-        }
-    </style>
 
     <!-- Header -->
-    <header class="bg-dark py-5">
+    <header class="bg-dark py-5" style="height: 86vh;
+        min-height: 500px;
+        background-image: url('{{asset('storage/images/background.png')}}');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;">
         <div class="container h-100">
             <div class="row h-100 align-items-center">
                 <div class="col-lg-12 text-center text-wrap text-light">
@@ -21,16 +16,17 @@
                     <h1 class="brand">Litnify</h1>
                     <h2>Das Suchportal der Meteorologiebibliothek</h2>
                     <form action="suche" method="get">
-                        <div class="form-row">
-                            <div class="col-12 col-md-9 mb-2 mb-md-0">
-                                <div class="input-group">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-primary fa fa-search rounded-left" type="button"></button>
+
+                        <div class="form-row ">
+                            <div class="col-md-9">
+                                <div class="row no-gutters">
+                                    <div class="col-md-7 input-group">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-primary fa fa-search rounded-left" type="button"></button>
+                                        </div>
+                                        <input type="text" name="q" class="form-control form-control-lg rounded-0" placeholder="Bitte Suchbegriff eingeben..." autofocus />
                                     </div>
-
-                                    <input type="text" name="q" class="form-control form-control-lg" placeholder="Bitte Suchbegriff eingeben..." autofocus />
-
-                                    <div class="input-group-append">
+                                    <div class="col-md-5">
                                         <select class="custom-select custom-select-lg rounded-0" name="filter">
                                             @foreach ($auswahl as $item)
                                                 <option value="{{($item['short'])}}">{{($item['full'])}}</option>
@@ -39,10 +35,11 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-12 col-md-3">
+                            <div class="col-md-3">
                                 <button class="btn btn-primary btn-block btn-lg" type="submit"> <i class="fa fa-search"></i> Suchen</button>
                             </div>
                         </div>
+
                     </form>
                 </div>
             </div>
