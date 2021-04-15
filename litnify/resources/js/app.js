3ff82a864a3aacd9df48eca4879839a8e5d4bf6f
svnwa <svnwal@gmail.com>
2021-04-15 23:20:10 +0200

    service worker console.log angepasst

--- litnify/resources/js/app.js
+++ litnify/resources/js/app.js
@@ -7,10 +7,10 @@ require('./daterangepicker')
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./service-worker.js')
         .then(function (reg){
-            console.log("Successfully registered service worker !");
+            console.log('Registration successful, scope is:', reg.scope);
         })
         .catch(function (err){
 
-            console.log("Service worker not registered!");
+            console.log('Service worker registration failed, error:', err);
         })
 }
